e0f40284a7f16edc0a08e28f414f716f3c8f8c24
Kevin Harper <keharper@adobe.com>
2023-11-03 10:11:30 -0500

    Add missing query

--- src/data/navigation/sections/graphql.js
+++ src/data/navigation/sections/graphql.js
@@ -7,42 +7,46 @@ module.exports = [
     title: "Catalog Service",
     path: "/graphql/catalog-service",
     pages:[
-        {
-            title: "categories query",
-            path: "/graphql/catalog-service/categories.md",
-        },
-        {
-            title: "products query",
-            path: "/graphql/catalog-service/products.md"
-        },
-        {
-            title: "productSearch query",
-            path: "/graphql/catalog-service/product-search.md",
-        }
+      {
+        title: "categories query",
+        path: "/graphql/catalog-service/categories.md",
+      },
+      {
+        title: "products query",
+        path: "/graphql/catalog-service/products.md"
+      },
+      {
+        title: "productSearch query",
+        path: "/graphql/catalog-service/product-search.md",
+      },
+      {
+        title: "refineProduct query",
+        path: "/graphql/catalog-service/refine-product.md"
+      }
     ]
   },
   {
     title: "Live Search",
     path: "/graphql/live-search",
     pages: [
-        {
-            title: "attributeMetadata query",
-            path: "/graphql/live-search/attribute-metadata.md"
-        },
-        {
-            title: "productSearch query",
-            path: "/graphql/live-search/product-search.md"
-        }
+      {
+        title: "attributeMetadata query",
+        path: "/graphql/live-search/attribute-metadata.md"
+      },
+      {
+        title: "productSearch query",
+        path: "/graphql/live-search/product-search.md"
+      }
     ]
   },
   {
     title: "Product Recommendations",
     path: "/graphql/recommendations/",
     pages: [
-        {
-            title: "recommendations query",
-            path: "graphql/recommendations/recommendations.md"
-        }
+      {
+        title: "recommendations query",
+        path: "graphql/recommendations/recommendations.md"
+      }
     ]
   },
 ];
