c5487049c9992bb530655e977a2fd3d3da6be53c
Don <dobooth@adobe.com>
2024-01-18 09:35:28 -0800

    fix path

--- src/data/navigation/sections/index.js
+++ src/data/navigation/sections/index.js
@@ -2,6 +2,6 @@ const live_search = require("./live_search");
 const product_recommendations  = require("./product_recommendations");
 const reporting = require("./reporting");
 const shared_services  = require("./shared_services");
-const feed = require("./feed_ingestion");
+const feed_ingestion = require("./feed_ingestion");
 
 module.exports = [...live_search, ...product_recommendations, ...reporting, ...shared_services, ...feed_ingestion];
