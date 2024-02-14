d5af964b8736e0de602f1959cefac96cbfc9d446
Jeff Matthews <jmatthew@adobe.com>
2024-02-13 18:11:29 -0600

    Reverted Data Ingestion REST API content

--- src/data/navigation/sections/index.js
+++ src/data/navigation/sections/index.js
@@ -3,6 +3,5 @@ const product_recommendations  = require("./product_recommendations");
 const reporting = require("./reporting");
 const shared_services  = require("./shared_services");
 const graphql = require("./graphql");
-const data_ingestion = require("./data_ingestion");
 
-module.exports = [...live_search, ...product_recommendations, ...reporting, ...shared_services, ...graphql, ...data_ingestion];
+module.exports = [...live_search, ...product_recommendations, ...reporting, ...shared_services, ...graphql];
--- src/pages/data-ingestion/data-ingestion.md
+++
@@ -1,5 +0,0 @@
----
-title: Adobe Commerce Data Ingestion REST endpoints
-description: Reference information for Adobe Commerce Data Ingestion REST endpoints.
-openAPISpec: /data-ingestion.yaml
----
