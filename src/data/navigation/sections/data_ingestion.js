c374c6af02c5e8d49ad4993688909880dc7d1531
Misha Slabko <mslabko@adobe.com>
2024-02-16 16:05:31 -0600

    Revert "Reverted Data Ingestion REST API content"
    This reverts commit d5af964b8736e0de602f1959cefac96cbfc9d446.

---
+++ src/data/navigation/sections/data_ingestion.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    title: "Data Ingestion REST API",
+    path: "/data-ingestion/",
+    pages:[
+      {
+        title: "Data Ingestion REST reference",
+        path: "/data-ingestion/data-ingestion.md",
+      }
+    ]
+  },
+];
