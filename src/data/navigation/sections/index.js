dbd507ee6df920f6b110af8aa09c75ba6f03c28c
jhadobe <jhoover@adobe.com>
2025-03-10 18:18:33 -0500

    remove tutorials from index.js

--- src/data/navigation/sections/index.js
+++ src/data/navigation/sections/index.js
@@ -5,7 +5,6 @@ const reporting = require("./reporting");
 const shared_services  = require("./shared_services");
 const graphql = require("./graphql");
 const cloud_service_guides = require("./cloud_service_guides");
-const cloud_service_tutorials = require("./cloud_service_tutorials");
 
 
-module.exports = [...live_search, ...product_recommendations, ...reporting, ...shared_services, ...graphql, ...cloud_service_guides, ...cloud_service_tutorials];
+module.exports = [...live_search, ...product_recommendations, ...reporting, ...shared_services, ...graphql, ...cloud_service_guides];
