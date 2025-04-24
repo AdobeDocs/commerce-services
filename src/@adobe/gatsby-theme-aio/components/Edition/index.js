71121e729886dea9c3744b15bd2005c1a4d880b5
Jeff Matthews <jmatthew@adobe.com>
2025-04-24 11:27:08 -0500

    fix: align paas tooltip with ExL

--- src/@adobe/gatsby-theme-aio/components/Edition/index.js
+++ src/@adobe/gatsby-theme-aio/components/Edition/index.js
@@ -26,7 +26,7 @@ const Edition = ({ ...props }) => {
     case 'paas':
       editionText = 'PaaS';
       editionColor = 'spectrum-Badge--informative';
-      editionTooltip = 'Applies to Adobe Commerce on Cloud (Adobe-managed PaaS infrastructure) and on-premises projects.';
+      editionTooltip = 'Applies to Adobe Commerce on Cloud (Adobe-managed PaaS infrastructure) and on-premises projects only.';
       break;
     case 'saas':
       editionText = 'SaaS';
