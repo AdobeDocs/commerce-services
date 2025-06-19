4b52e0e55ed1470a0221aa1d404a357d9c20d2a1
Kevin Harper <keharper@adobe.com>
2025-06-19 09:35:00 -0500

    Remove ACCS and graphql content

--- src/data/navigation/header.js
+++ src/data/navigation/header.js
@@ -4,31 +4,6 @@ module.exports = [
     path: "/",
   },
   {
-    title: "Guides",
-    menu: [
-      {
-        title: "Commerce as a Cloud Service",
-        description: "Learn the basics of using the Commerce as a Cloud Service APIs.",
-        path: "/cloud/guides/",
-      },
-      {
-        title: "Catalog Service",
-        description: "Learn how Catalog Service implements GraphQL.",
-        path: "/graphql/catalog-service/",
-      },
-      {
-        title: "Live Search",
-        description: "Learn how Live Search implements GraphQL.",
-        path: "/graphql/live-search/"
-      },
-      {
-        title: "Product Recommendations",
-        description: "Learn how Product Recommendations implements GraphQL.",
-        path: "/graphql/recommendations/"
-      }
-    ],
-  },
-  {
     title: "API Reference",
     menu: [
       {
