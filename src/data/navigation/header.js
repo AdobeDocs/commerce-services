3913910e0cef056bdd69466a530e332ecc1d199d
Margaret Eker <meker@adobe.com>
2025-06-25 14:19:27 -0500

    - Fix file name -Fix header links

--- src/data/navigation/header.js
+++ src/data/navigation/header.js
@@ -12,11 +12,11 @@ module.exports = [
     menu: [
       {
         title: "Catalog data ingestion REST API",
-        path: "/optimizer/reference/rest"
+        path: "/optimizer/reference/rest/data-ingestion-api.md"
       },
       {
         title: "Merchandising GraphQL API",
-        path: "/optimizer/reference/graphql"
+        path: "/optimizer/reference/graphql/merchandising-api.md"
       },
     ]
   },
