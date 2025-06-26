b5f10899279a7cd090ba4452b82c8e4eee6e8ed4
Margaret Eker <meker@adobe.com>
2025-06-26 15:56:26 -0500

    Add navigation index

---
+++ src/data/navigation/sections/reference.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    title: "Reference",
+    path: "/reference/",
+    pages: [
+      {
+        title: "Data Ingestion API",
+        path: "/rest/",
+      },
+      {
+        title: "Merchandising API",
+        path: "/graphql/",
+      },
+    ],
+  },
+];
