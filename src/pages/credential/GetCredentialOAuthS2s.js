ceb39f99620eadc82fe213853e60d3a533f837f8
Jeff Matthews <jmatthew@adobe.com>
2025-08-07 10:50:29 -0500

    feat: replace staging template ID and server URL with production sandbox

--- src/pages/credential/GetCredentialOAuthS2s.js
+++ src/pages/credential/GetCredentialOAuthS2s.js
@@ -6,7 +6,7 @@ const GetCredentialOAuthS2s = () => {
 
   return (
 
-    <GetCredential className="getCredentialContainer" templateId="6887db51a6867b6202ef2f9c" productName='Adobe Commerce Optimizer' >
+    <GetCredential className="getCredentialContainer" templateId="6887db9dbaf36ca1f2c4539b" productName='Adobe Commerce Optimizer' >
 
       <GetCredential.SignIn title="Get credentials" paragraph="After signing in, you can create credentials that can be used to call the catalog data ingestion REST API." buttonText="Sign in" />
 
